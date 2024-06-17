ec257fdf01f47a2ecf16aecd087f69445c4e6cee
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-17 22:38:25 +0600

    simple title bar added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { FaFacebook, FaTwitter, FaLinkedinIn, FaDiscord } from "react-icons/fa6";
 
 
 const Navbar = () => {
@@ -7,16 +8,21 @@ const Navbar = () => {
             {/* section that will hold everything in this page */}
             <section>
                 {/* section for date time and social links */}
-                    <section className="bg-[#31473A] w-full h-7">
-                        {/* date and time */}
-                        <p className='text-white'>
+                <section className="bg-[#31473A] flex justify-between items-center w-full h-7">
+                    {/* date and time */}
+                    <p className='text-white'>
                         {moment().format("dddd, MMM Do, YYYY")}
-                        </p>
-                        {/* social links */}
-                        <p>
-
-                        </p>
-                    </section>
+                    </p>
+                    {/* social links */}
+                    <p>
+                        <ul className='flex gap-5 text-white'>
+                            <li><FaFacebook /></li>
+                            <li><FaTwitter /></li>
+                            <li><FaLinkedinIn /></li>
+                            <li><FaDiscord  /></li>
+                        </ul>
+                    </p>
+                </section>
             </section>
         </>
     );
