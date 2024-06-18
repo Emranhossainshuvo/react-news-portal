2402889e5645f508793dd49d356247a06acde796
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-18 08:50:00 +0600

    added error page for handling false routes path

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,12 +4,14 @@ import {
 import Home from "../pages/Home/Home";
 import Main from "../Main/Main";
 import NewsDetailPage from "../pages/NewsDetailPage/NewsDetailPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./error.css"
+
+const ErrorPage = () => {
+    return (
+        <>
+            <div className="grid h-screen px-4 bg-white place-content-center">
+                <div className="text-center">
+                    <img
+                        className="md:max-w-[60vw]"
+                        src="https://i.ibb.co/zZRCCxp/20824298-6342464.jpg"
+                        alt=""
+                    />
+
+
+                    <Link to='/'>
+                        <button className="btn text-[#1b2e35] font-bold text-1xl bg-[#aeedcf] hover:bg-[#c7f1dc] hover:shadow-lg mt-10">Home</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ErrorPage;
