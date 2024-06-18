faa6acf0ddc9d0a7c72e1300119dc820b932566f
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-18 08:45:31 +0600

    created a basic news detail page

--- src/pages/NewsDetailPage/NewsDetailPage.jsx
+++ src/pages/NewsDetailPage/NewsDetailPage.jsx
@@ -1,9 +1,15 @@
 
 const NewsDetailPage = () => {
     return (
-        <div>
-            <h4>News detail page</h4>            
-        </div>
+        <>
+            {/* section to hold everything in this page */}
+            <section className="mt-10">
+                <img className="" src="https://i.ibb.co/pK09t9s/luca-bravo-XJXWbf-So2f0-unsplash.jpg" alt="" />
+
+                <h4 className="text-3xl font-semibold mt-7">Coding is the hapiness for some Young people </h4>
+                <p className="mt-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, ducimus? Recusandae, fuga ea deleniti placeat repellat aliquam molestias beatae tempora quo natus optio! Veritatis consequatur, inventore aperiam optio in magni? Minima rerum aliquid delectus maxime vero perspiciatis libero cupiditate! Sequi tenetur sit quaerat, odio magni quia officia a voluptatem sed laboriosam saepe quae, numquam, quisquam possimus cupiditate exercitationem neque ipsum!</p>
+            </section>
+        </>
     );
 };
 
